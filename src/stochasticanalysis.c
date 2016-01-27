6ab51b09de0fae26eec66ebd134900f69aefb904
Liuga92 <luc.92@hotmail.it>
2016-01-27 17:06:05 +0100

    splitted stochasticanalysis

--- src/stochasticanalysis.c
+++ src/stochasticanalysis.c
@@ -2,36 +2,18 @@
 #include "stochasticanalysis.h"
 
 #include <stdlib.h>
+#include <string.h>
 #include "convolutions.h"
+#include "algebricoperations.h"
+#include "ioutils.h"
 
-/*rerurns the hyperperiod of a taskset*/
-static unsigned int get_hyperperiod (stochastic_taskset * ts);
-/*computes lowest common multiple*/
-static unsigned int get_lcm (unsigned int a, unsigned int b);
-/*iterates over the tasks of a task_schedule over the hyperperiod,
-setting the timeline struct inside t_sched to the id of the
-next job that has to start and the relative (respect to the
-hyperperiod) starting time*/
-static int next_task (tasks_schedule * t_sched);
-/*computes the mean of a distribution*/
-static double distr_mean (double *distribution, size_t distr_len);
+
+
 /*
-gets the maximum idle time << called "r" inside the paper >>
-of a taskset (inside a task schedule)
-with fixed priorities, respect to the kth task of the taskset.
+get the last index whose value is different from 0 inside a distributon
 */
-static unsigned int get_max_idle_FP (tasks_schedule * t_sched,
-				     unsigned int kth_task);
-/*
-get the first index whose value is different from 0 inside a distributon
-*/
-static unsigned int min_index (double *distribution, size_t d_len);
-/*
-check in a taskset with fixed priorities
-task "this" has priority respect to the task "over_this".
-*/
-static int has_priority_FP (stochastic_taskset * ts, unsigned int this,
-			 unsigned int over_this);
+static unsigned int max_index (double *distribution, size_t d_len);
+
 /*
 computes the baklog distribution starting from "starting_dist" (if different
 from null) for task with id "task_id" (inside the taskset),
@@ -46,375 +28,241 @@ static stochastic_distribution *compute_backlog_until (tasks_schedule *
 						       stochastic_distribution
 						       * starting_dist);
 
-/*
-returns a task schedule object for the stochastic taskset "ts"
-*/
-tasks_schedule *
-new_task_schedule (stochastic_taskset * ts)
-{
-  tasks_schedule *t_sched;
-
-  if (!ts)
-    return NULL;
-
-  t_sched = malloc (sizeof (tasks_schedule));
-
-  if (!t_sched)
-    return NULL;
-  t_sched->ts = ts;
-  t_sched->hyperperiod = get_hyperperiod (ts);
-
-  {
-    int i;
-    unsigned int t_num = 0;
-
-    for (i = 0; i < ts->tasks_num; i++)
-      {
-	t_num += t_sched->hyperperiod / ts->task_list[i]->period;
-      }
-    t_sched->timeline_len = t_num;
-  }
-
-  t_sched->timeline.is_new = 1;
-  get_max_idle_FP (t_sched, 0);
-
-  return t_sched;
-}
-
-/*
-frees the memory of the task schedule passed AND THE ASSOCIATED TASKSET
-*/
-void
-free_task_schedule (tasks_schedule * t_sched)
-{
-  if (!t_sched)
-    return;
-
-  free_stochastic_taskset (t_sched->ts);
-  free (t_sched);
-}
-
-int
-is_stable (tasks_schedule * t_sched)
-{
-  int i;
-  double average_utilization = 0.0;
-  for (i = 0; i < t_sched->ts->tasks_num; i++)
-    {
-      average_utilization +=
-	distr_mean (t_sched->ts->task_list[i]->sd.dist,
-		    t_sched->ts->task_list[i]->sd.d_len) /
-	t_sched->ts->task_list[i]->period;
-    }
-  printf ("average utilization %lf\n", average_utilization);
-  if (average_utilization <= 1)
-    return 1;
-  else
-    return 0;
-}
-
 
 /*
 computes the backlog matrix for the kth task. The size parameter is the
 dimension of the matrix. The minimum is the one returned by the function
 "get_minimum_matrix_size"
 */
-backlog_matrix *get_backlog_matrix_of_size (tasks_schedule * t_sched,
-					    unsigned int kth_task,
-					    size_t size);
+backlog_matrix *
+get_backlog_matrix_of_size (tasks_schedule * t_sched,
+			    unsigned int kth_task, size_t size)
+{
+
+  backlog_matrix *b_mat;
+  /*if the size is not enough, abort */
+  {
+    size_t min_size;
+
+    min_size = get_minimum_matrix_size (t_sched, kth_task);
+    if (size < min_size)
+      {
+	printf ("size too small, minimum size %zu\n", min_size);
+      }
+  }
+  /*allocating and initializing the matrix */
+  b_mat = malloc (sizeof (backlog_matrix));
+  if (!b_mat)
+    return NULL;
+  b_mat->matrix = calloc (size * size, sizeof (double));
+  if (!b_mat->matrix)
+    {
+      free (b_mat);
+      return NULL;
+    }
+  memset (b_mat->matrix, 0, size * size * sizeof (double));
+
+  b_mat->dimension = size;
+  b_mat->id = kth_task;
+  b_mat->sched = t_sched;
+
+  {
+    unsigned int r, m_r;
+    stochastic_distribution *b_r;
+    stochastic_distribution *r_d;
+    /*getting r */
+    r = get_max_idle_FP (b_mat->sched, b_mat->id);
+    r_d = new_stochastic_distribution (r + 1);
+    if (!r_d)
+      {
+	free_backlog_matrix (b_mat);
+	return NULL;
+      }
+    r_d->dist[r] = 1.0;
+    b_r = compute_backlog_until (b_mat->sched, b_mat->id,
+				 b_mat->sched->hyperperiod, r_d);
+    if (!b_r)
+      {
+	free_backlog_matrix (b_mat);
+	free_stochastic_distribution (r_d);
+	return NULL;
+      }
+
+    m_r = max_index (b_r->dist, b_r->d_len);
+
+    {
+      /*filling the matrix from to r up the last */
+      unsigned int i;
+      /*for every column  >= r fill the rows with b_r */
+      for (i = 0; i < b_mat->dimension - r; i++)
+	{
+	  unsigned int j;
+	  for (j = 0; j <= m_r; j++)
+	    {
+	      /*if the is inside the truncated matrix */
+	      if (j + i < b_mat->dimension)
+		{
+
+		  b_mat->matrix[(i + r) + (i + j) * size] = b_r->dist[j];
+		}
+	      /*if not, accumulate teh probability at the highest index */
+	      else
+		{
+		  b_mat->matrix[(size - 1) * size + i + r] += b_r->dist[j];
+		}
+	    }
+	}
+    }
+    /*fill the matrix from row 0 to r-1 */
+    free_stochastic_distribution (b_r);
+    {
+      int i, j;
+      stochastic_distribution *b_i;
+      for (i = 0; i < r; i++)
+	{
+	  memset (r_d->dist, 0, r_d->d_len * sizeof (double));
+	  r_d->dist[i] = 1;
+
+	  b_i = compute_backlog_until (b_mat->sched, b_mat->id,
+				       b_mat->sched->hyperperiod, r_d);
+
+	  if (!b_r)
+	    {
+	      free_backlog_matrix (b_mat);
+	      free_stochastic_distribution (r_d);
+	      return NULL;
+	    }
+
+	  for (j = 0; j < b_i->d_len && j <= m_r; j++)
+	    {
+	      b_mat->matrix[b_mat->dimension * j + i] = b_i->dist[j];
+	    }
+
+	  free_stochastic_distribution (b_i);
+
+	}
+    }
+
+    free_stochastic_distribution (r_d);
+  }
+
+  return b_mat;
+
+}
 
 /*
 frees the memory taken by the matrix NOT THE ONE OF THE ASSOCIATED TASK SHEDULE
 */
-void free_backlog_matrix (backlog_matrix * bk_mat);
+void
+free_backlog_matrix (backlog_matrix * bk_mat)
+{
+  free (bk_mat->matrix);
+  free (bk_mat);
+
+}
+
 /*
 Get the minimum dimension for the backlog matrix for kth process.
 */
-size_t get_minimum_matrix_size (tasks_schedule * t_sched,
-				unsigned int kth_task)
+size_t
+get_minimum_matrix_size (tasks_schedule * t_sched, unsigned int kth_task)
 {
-	unsigned int r,mr;
-	stochastic_distribution * sd;
-	stochastic_distribution * bkr_sd;
+  unsigned int r, mr;
+  stochastic_distribution *sd;
+  stochastic_distribution *bkr_sd;
 
-	/* getting maximum idle time*/
-	r = get_max_idle_FP(t_sched,kth_task);
+  /* getting maximum idle time */
+  r = get_max_idle_FP (t_sched, kth_task);
 
-	bkr_sd = new_stochastic_distribution(r +1);
-	if(! bkr_sd) return 0;
+  bkr_sd = new_stochastic_distribution (r + 1);
+  if (!bkr_sd)
+    return 0;
 
-	bkr_sd->dist[r] = 1.0;
-	/*getting b_r (colunm r of the backlog matrix)*/
-	sd = compute_backlog_until(t_sched,kth_task,t_sched->hyperperiod,bkr_sd);
+  bkr_sd->dist[r] = 1.0;
+  /*getting b_r (colunm r of the backlog matrix) */
+  sd =
+    compute_backlog_until (t_sched, kth_task, t_sched->hyperperiod, bkr_sd);
 
-	if(! sd){
-		free_stochastic_distribution(bkr_sd);
-		return 0;
-	}
+  if (!sd)
+    {
+      free_stochastic_distribution (bkr_sd);
+      return 0;
+    }
 
-	/*computing m_r (last nonzero cell fo the column b_r)*/
-	{
-		int i,found;
-		found = 0;
-		for(i=sd->d_len-1;i>=0 && found == 0;i--){
-			if(sd->dist[i] >0) {
-				found = 1;
-				mr = i;
-				}
-		}
-	}
+  /*computing m_r (last nonzero cell fo the column b_r) */
+  {
+    int i, found;
+    found = 0;
+    for (i = sd->d_len - 1; i >= 0 && found == 0; i--)
+      {
+	if (sd->dist[i] > 10e-6)
+	  {
+	    found = 1;
+	    mr = i;
+	  }
+      }
+  }
 
-	free_stochastic_distribution(sd);
-	free_stochastic_distribution(bkr_sd);
+  free_stochastic_distribution (sd);
+  free_stochastic_distribution (bkr_sd);
 
-	/*returning the bigger*/
-	if (r > mr) return r + 1;
-	else return mr + 1;
+  /*returning the bigger */
+  if (r > mr)
+    return r + 1;
+  else
+    return mr + 1;
 
 }
+
 /*
 get an array containing the response time distribution for the given
 backlog matrix (that is associated with a specific task). The distribution
 is correct UP TO THE DEADLINE. The remaining probability is collapsed in
 the time DEADLINE + 1.
 */
-stochastic_distribution *get_response_time_of (backlog_matrix * b_mat);
-
-
-
-static unsigned int
-get_hyperperiod (stochastic_taskset * ts)
+stochastic_distribution *
+get_stationary_distribution (backlog_matrix * b_mat)
 {
-  unsigned int hyperperiod = 0;
-  unsigned int i = 0;
 
-  if (ts->tasks_num == 1)
-    {
-      return ts->task_list[0]->period;
-    }
+  stochastic_distribution *b_stationary;
 
-  for (i = 1; i < ts->tasks_num; i++)
-    {
+  b_stationary = new_stochastic_distribution (b_mat->dimension);
+  if (!b_stationary)
+    return NULL;
 
-      if (i == 1)
-	{
-	  hyperperiod =
-	    get_lcm (ts->task_list[0]->period, ts->task_list[1]->period);
-	}
-      else
-	{
-	  hyperperiod = get_lcm (hyperperiod, ts->task_list[i]->period);
-	}
-    }
-
-  return hyperperiod;
-
-}
-
-static unsigned int
-get_lcm (unsigned int a, unsigned int b)
-{
-  int tmp;
-
-  tmp = (a > b) ? a : b;
-
-  while ((tmp % a != 0) || (tmp % b != 0))
-    {
-      tmp++;
-    }
-
-  return tmp;
-
-
-
-}
-
-/*
-iterates over the hyperperiod, setting the timeline struct to the next activation
-of the next process. returns 1 if there's another process that needs to
-be scheduled in the hyperperiod, 0 otherwise.
-*/
-static int
-next_task (tasks_schedule * t_sched)
-{
-  unsigned int current_time;
-  unsigned int current_task;
-
-
-
-  if (t_sched->timeline.is_new)
-    {
-      current_time = 0;
-      current_task = 0;
-      t_sched->timeline.is_new = 0;
-    }
-  else
-    {
-      if (t_sched->ts->tasks_num == t_sched->timeline.id + 1)
-	{
-	  current_time = t_sched->timeline.release_time + 1;
-	  current_task = 0;
-	}
-      else
-	{
-	  current_time = t_sched->timeline.release_time;
-	  current_task = t_sched->timeline.id + 1;
-	}
-    }
+  algop_get_best_eigen (b_mat->matrix, b_mat->dimension, b_stationary->dist);
 
   {
-
-    int go_ahead = 1;
-    unsigned int i;
-    unsigned int task_copy = current_task;
-    for (i = current_time; go_ahead && i < t_sched->hyperperiod; i++)
+    int i;
+    double subm = 0.0;
+    for (i = 0; i < b_stationary->d_len; i++)
       {
-	unsigned int j;
-	for (j = task_copy; go_ahead && j < t_sched->ts->tasks_num; j++)
-	  {
-	    stochastic_task *t;
-	    t = t_sched->ts->task_list[j];
-
-	    if ((i % t->period) - t->activation_time == 0)
-	      {
-		go_ahead = 0;
-		current_task = j;
-		current_time = i;
-	      }
-	  }
-
-	task_copy = 0;
+	subm += b_stationary->dist[i];
       }
-
-    if (go_ahead)
+    for (i = 0; i < b_stationary->d_len; i++)
       {
-	t_sched->timeline.is_new = 1;
-	return 0;
+	b_stationary->dist[i] /= subm;
       }
-    else
-      {
-	t_sched->timeline.id = current_task;
-	t_sched->timeline.release_time = current_time;
-	return 1;
-      }
-
-
   }
+  return b_stationary;
 
 }
 
-
-
-static double
-distr_mean (double *distribution, size_t distr_len)
+static unsigned int
+max_index (double *distribution, size_t d_len)
 {
   int i;
-  double mean = 0.0;
 
-  for (i = 0; i < distr_len; i++)
-    {
-      mean += i * distribution[i];
-    }
-
-  return mean;
-
-}
-
-static unsigned int
-get_max_idle_FP (tasks_schedule * t_sched, unsigned int kth_task)
-{
-  unsigned int max_idle = 0;
-  task_timeline *tl;
-
-  tl = &t_sched->timeline;
-  max_idle += t_sched->hyperperiod;
-
-  if (kth_task >= t_sched->ts->tasks_num)
-    {
-      printf ("error! computing max idle of task %i but task len is %zu\n",
-	      kth_task, t_sched->ts->tasks_num);
-    }
-
-	/*computing W_min*/
-  {
-    unsigned int W_min = 0;
-    unsigned int prev_time, prev_duration;
-
-    do
-      {
-	next_task (t_sched);
-      }
-    while (has_priority_FP (t_sched->ts, kth_task, tl->id));
-
-    prev_time = t_sched->timeline.release_time;
-    prev_duration =
-      min_index (t_sched->ts->task_list[tl->id]->sd.dist,
-		 t_sched->ts->task_list[tl->id]->sd.d_len);
-    W_min = prev_duration;
-
-    while (next_task (t_sched))
-      {
-	if (!has_priority_FP (t_sched->ts, kth_task, tl->id))
-	  {
-	    W_min = (W_min > tl->release_time - prev_time) ?
-	      W_min - (tl->release_time - prev_time) : 0;
-
-	    prev_duration =
-	      min_index (t_sched->ts->task_list[tl->id]->sd.dist,
-			 t_sched->ts->task_list[tl->id]->sd.d_len);
-
-	    prev_time = t_sched->timeline.release_time;
-	    W_min += prev_duration;
-	  }
-      }
-    W_min = (W_min > t_sched->hyperperiod - prev_time) ?
-      W_min - (t_sched->hyperperiod - prev_time) : 0;
-
-    max_idle += W_min;
-  }
-	/*computing C_min*/
-  {
-    unsigned int C_min = 0;
-    while (next_task (t_sched))
-      {
-	if (!has_priority_FP (t_sched->ts, kth_task, tl->id))
-	  {
-	    C_min += min_index (t_sched->ts->task_list[tl->id]->sd.dist,
-				t_sched->ts->task_list[tl->id]->sd.d_len);
-	  }
-
-      }
-    max_idle -= C_min;
-  }
-
-
-  return max_idle;
-}
-
-static int
-has_priority_FP (stochastic_taskset * ts, unsigned int this,
-	      unsigned int over_this)
-{
-  if (ts->task_list[this]->deadline < ts->task_list[over_this]->deadline)
-    return 1;
-  else
-    return 0;
-}
-
-static unsigned int
-min_index (double *distribution, size_t d_len)
-{
-  unsigned int i;
-
-  for (i = 0; i < d_len; i++)
+  for (i = d_len - 1; i >= 0; i--)
     {
       if (distribution[i] > 0)
-	return i;
+	{
+	  return i;
+	}
     }
 
   return 0;
 }
-
 
 
 static stochastic_distribution *
@@ -426,13 +274,13 @@ compute_backlog_until (tasks_schedule *
   stochastic_distribution *start;
   size_t previous_time;
 
-	/*checking if there's a starting distribution*/
+  /*checking if there's a starting distribution */
   if (starting_dist)
     {
       start = copy_stochastic_distribution (starting_dist);
       previous_time = 0;
     }
-	/*if not, loading the first task as starting distribution*/
+  /*if not, loading the first task as starting distribution */
   else
     {
       do
@@ -442,23 +290,27 @@ compute_backlog_until (tasks_schedule *
 	}
       while (has_priority_FP (t_sched->ts, task_id, t_sched->timeline.id));
       start =
-	copy_stochastic_distribution (&t_sched->ts->
-				      task_list[t_sched->timeline.id]->sd);
+	copy_stochastic_distribution (&t_sched->
+				      ts->task_list[t_sched->timeline.id]->
+				      sd);
       previous_time = t_sched->timeline.release_time;
 
     }
-	/*while there's task and the time is not expired, convolve shrink the next task*/
+  /*while there's task and the time is not expired, convolve shrink the next task */
   while (next_task (t_sched) && (t_sched->timeline.release_time < until_time))
     {
       if (!has_priority_FP (t_sched->ts, task_id, t_sched->timeline.id))
 	{
 	  d = convolve_shrink_SD (start,
-				  &t_sched->ts->task_list[t_sched->timeline.id]->sd,
-				  t_sched->timeline.release_time - previous_time);
+				  &t_sched->ts->task_list[t_sched->timeline.
+							  id]->sd,
+				  t_sched->timeline.release_time -
+				  previous_time);
 
-	  if (!d){
-      return NULL;
-      }
+	  if (!d)
+	    {
+	      return NULL;
+	    }
 
 	  previous_time = t_sched->timeline.release_time;
 	  free_stochastic_distribution (start);
@@ -466,8 +318,9 @@ compute_backlog_until (tasks_schedule *
 
 	}
     }
-	/*shrink to cover the gap between the last released time and the target time*/
-	convolutions_shrink(d->dist,d->d_len,until_time - t_sched->timeline.release_time);
+  /*shrink to cover the gap between the last released time and the target time */
+  convolutions_shrink (d->dist, d->d_len,
+		       until_time - t_sched->timeline.release_time);
 
   return d;
 }
--- src/stochasticanalysis.h
+++ src/stochasticanalysis.h
@@ -3,52 +3,21 @@
 #define SRTTANALYSIS_STOCHASTICANALYSIS_H
 
 #include "stochastictasks.h"
+#include "schedulingutils.h"
 #include <stdlib.h>
 
-/*struct that contains data to iterato over the hyperperiod on the taskset*/
-typedef struct
-{
-  int is_new; /*is a new iteration*/
-  unsigned int id; /*id of the next task to activate*/
-  unsigned int release_time; /*releasing time WRT the hyperperiod*/
-} task_timeline;
 
-/*
-struct representing a schedule of a taskset
-*/
-typedef struct
-{
-  unsigned int hyperperiod; /*hyperperiod*/
-  unsigned int timeline_len; /*number of tasks activated during a hyperperiod*/
-  task_timeline timeline;
-  stochastic_taskset *ts;
-} tasks_schedule;
 
 /*backlog matrix for a certain task*/
 typedef struct
 {
-  size_t len; /*len of matrix*/
+  size_t dimension;		/*len of matrix */
   double *matrix;
-  unsigned int id; /*id of the task*/
-  tasks_schedule * sched; /*linked task schedule*/
+  unsigned int id;		/*id of the task */
+  tasks_schedule *sched;	/*linked task schedule */
 } backlog_matrix;
 
 
-
-/*
-returns a task schedule object for the stochastic taskset "ts"
-*/
-tasks_schedule *new_task_schedule (stochastic_taskset * ts);
-
-/*
-frees the memory of the task schedule passed AND THE ASSOCIATED TASKSET
-*/
-void free_task_schedule (tasks_schedule * t_sched);
-
-/*
-Check if the system is stable (if the average system utilization is < 1)
-*/
-int is_stable (tasks_schedule * t_sched);
 /*
 computes the backlog matrix for the kth task. The size parameter is the
 dimension (number of rows/columns) of the matrix.
@@ -70,12 +39,12 @@ for the backlog matrix for kth process.
 size_t get_minimum_matrix_size (tasks_schedule * t_sched,
 				unsigned int kth_task);
 /*
-get an array containing the response time distribution for the given
+get an array containing the stationary distribution for the given
 backlog matrix (that is associated with a specific task). The distribution
 is correct UP TO THE DEADLINE. The remaining probability is collapsed in
 the time DEADLINE + 1.
 */
-stochastic_distribution *get_response_time_of (backlog_matrix  *  b_mat);
+stochastic_distribution *get_stationary_distribution (backlog_matrix * b_mat);
 
 
 
