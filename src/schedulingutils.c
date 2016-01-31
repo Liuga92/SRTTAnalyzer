628a07ad584a9b8a7de23d0931b181ae557dfb55
Liuga92 <luc.92@hotmail.it>
2016-01-31 15:14:10 +0100

    moved some functions from others files  into these files

--- src/schedulingutils.c
+++ src/schedulingutils.c
@@ -5,20 +5,16 @@
 #include <string.h>
 #include "convolutions.h"
 #include "algebricoperations.h"
-#include "ioutils.h"
 
 /*rerurns the hyperperiod of a taskset*/
-static unsigned int get_hyperperiod (stochastic_taskset * ts);
+static unsigned int get_hyperperiod(stochastic_taskset * ts);
 /*computes lowest common multiple*/
-static unsigned int get_lcm (unsigned int a, unsigned int b);
+static unsigned int get_lcm(unsigned int a, unsigned int b);
 
 /*computes the mean of a distribution*/
-static double distr_mean (double *distribution, size_t distr_len);
+static double distr_mean(double *distribution, size_t distr_len);
 
-/*
-get the first index whose value is different from 0 inside a distributon
-*/
-static unsigned int min_index (double *distribution, size_t d_len);
+
 
 
 
@@ -26,116 +22,110 @@ static unsigned int min_index (double *distribution, size_t d_len);
 /*
 returns a task schedule object for the stochastic taskset "ts"
 */
-tasks_schedule *
-new_task_schedule (stochastic_taskset * ts)
+tasks_schedule *new_task_schedule(stochastic_taskset * ts)
 {
-  tasks_schedule *t_sched;
+    tasks_schedule *t_sched;
 
-  if (!ts)
-    return NULL;
+    if (!ts)
+	return NULL;
 
-  t_sched = malloc (sizeof (tasks_schedule));
+    t_sched = malloc(sizeof(tasks_schedule));
 
-  if (!t_sched)
-    return NULL;
-  t_sched->ts = ts;
-  t_sched->hyperperiod = get_hyperperiod (ts);
+    if (!t_sched)
+	return NULL;
+    t_sched->ts = ts;
+    t_sched->hyperperiod = get_hyperperiod(ts);
 
-  {
-    int i;
-    unsigned int t_num = 0;
+    {
+	int i;
+	unsigned int t_num = 0;
 
-    for (i = 0; i < ts->tasks_num; i++)
-      {
-	t_num += t_sched->hyperperiod / ts->task_list[i]->period;
-      }
-    t_sched->timeline_len = t_num;
-  }
+	for (i = 0; i < ts->tasks_num; i++) {
+	    t_num += t_sched->hyperperiod / ts->task_list[i]->period;
+	}
+	t_sched->timeline_len = t_num;
+    }
 
-  t_sched->timeline.is_new = 1;
-  get_max_idle_FP (t_sched, 0);
+    t_sched->timeline.is_new = 1;
+    get_max_idle_FP(t_sched, 0);
 
-  return t_sched;
+    return t_sched;
 }
 
 /*
 frees the memory of the task schedule passed AND THE ASSOCIATED TASKSET
 */
-void
-free_task_schedule (tasks_schedule * t_sched)
+void free_task_schedule(tasks_schedule * t_sched)
 {
-  if (!t_sched)
-    return;
+    if (!t_sched)
+	return;
 
-  free_stochastic_taskset (t_sched->ts);
-  free (t_sched);
+    free_stochastic_taskset(t_sched->ts);
+    free(t_sched);
 }
 
-int
-is_stable (tasks_schedule * t_sched)
+int is_stable(tasks_schedule * t_sched)
 {
-  int i;
-  double average_utilization = 0.0;
-  for (i = 0; i < t_sched->ts->tasks_num; i++)
-    {
-      average_utilization +=
-	distr_mean (t_sched->ts->task_list[i]->sd.dist,
-		    t_sched->ts->task_list[i]->sd.d_len) /
-	t_sched->ts->task_list[i]->period;
-    }
-  printf ("average utilization %lf\n", average_utilization);
-  if (average_utilization <= 1)
-    return 1;
-  else
-    return 0;
+    double average_utilization;
+
+    average_utilization = get_average_utilization(t_sched->ts);
+
+    if (average_utilization <= 1)
+	return 1;
+    else
+	return 0;
 }
 
 
-
-
-
-static unsigned int
-get_hyperperiod (stochastic_taskset * ts)
+double get_average_utilization(stochastic_taskset * ts)
 {
-  unsigned int hyperperiod = 0;
-  unsigned int i = 0;
+    int i;
+    double avg_util = 0.0;
+    for (i = 0; i < ts->tasks_num; i++) {
+	stochastic_task *t;
 
-  if (ts->tasks_num == 1)
-    {
-      return ts->task_list[0]->period;
+	t = ts->task_list[i];
+	avg_util += distr_mean(t->sd.dist, t->sd.d_len) / t->period;
+    }
+    return avg_util;
+}
+
+
+static unsigned int get_hyperperiod(stochastic_taskset * ts)
+{
+    unsigned int hyperperiod = 0;
+    unsigned int i = 0;
+
+    if (ts->tasks_num == 1) {
+	return ts->task_list[0]->period;
     }
 
-  for (i = 1; i < ts->tasks_num; i++)
-    {
+    for (i = 1; i < ts->tasks_num; i++) {
 
-      if (i == 1)
-	{
-	  hyperperiod =
-	    get_lcm (ts->task_list[0]->period, ts->task_list[1]->period);
-	}
-      else
-	{
-	  hyperperiod = get_lcm (hyperperiod, ts->task_list[i]->period);
+	if (i == 1) {
+	    hyperperiod =
+		get_lcm(ts->task_list[0]->period,
+			ts->task_list[1]->period);
+	} else {
+	    hyperperiod = get_lcm(hyperperiod, ts->task_list[i]->period);
 	}
     }
 
-  return hyperperiod;
+    return hyperperiod;
 
 }
 
-static unsigned int
-get_lcm (unsigned int a, unsigned int b)
+static unsigned int get_lcm(unsigned int a, unsigned int b)
 {
-  int tmp;
+    int tmp;
 
-  tmp = (a > b) ? a : b;
+    tmp = (a > b) ? a : b;
 
-  while ((tmp % a != 0) || (tmp % b != 0))
-    {
-      tmp++;
+    while ((tmp % a != 0) || (tmp % b != 0)) {
+	tmp++;
     }
 
-  return tmp;
+    return tmp;
 
 
 
@@ -146,185 +136,142 @@ iterates over the hyperperiod, setting the timeline struct to the next activatio
 of the next process. returns 1 if there's another process that needs to
 be scheduled in the hyperperiod, 0 otherwise.
 */
-int
-next_task (tasks_schedule * t_sched)
+int next_task(tasks_schedule * t_sched)
 {
-  unsigned int current_time;
-  unsigned int current_task;
+    unsigned int current_time;
+    unsigned int current_task;
 
 
 
-  if (t_sched->timeline.is_new)
-    {
-      current_time = 0;
-      current_task = 0;
-      t_sched->timeline.is_new = 0;
-    }
-  else
-    {
-      if (t_sched->ts->tasks_num == t_sched->timeline.id + 1)
-	{
-	  current_time = t_sched->timeline.release_time + 1;
-	  current_task = 0;
-	}
-      else
-	{
-	  current_time = t_sched->timeline.release_time;
-	  current_task = t_sched->timeline.id + 1;
+    if (t_sched->timeline.is_new) {
+	current_time = 0;
+	current_task = 0;
+	t_sched->timeline.is_new = 0;
+    } else {
+	if (t_sched->ts->tasks_num == t_sched->timeline.id + 1) {
+	    current_time = t_sched->timeline.release_time + 1;
+	    current_task = 0;
+	} else {
+	    current_time = t_sched->timeline.release_time;
+	    current_task = t_sched->timeline.id + 1;
 	}
     }
 
-  {
+    {
 
-    int go_ahead = 1;
-    unsigned int i;
-    unsigned int task_copy = current_task;
-    for (i = current_time; go_ahead && i < t_sched->hyperperiod; i++)
-      {
-	unsigned int j;
-	for (j = task_copy; go_ahead && j < t_sched->ts->tasks_num; j++)
-	  {
-	    stochastic_task *t;
-	    t = t_sched->ts->task_list[j];
+	int go_ahead = 1;
+	unsigned int i;
+	unsigned int task_copy = current_task;
+	for (i = current_time; go_ahead && i < t_sched->hyperperiod - 1;
+	     i++) {
+	    unsigned int j;
+	    for (j = task_copy; go_ahead && j < t_sched->ts->tasks_num;
+		 j++) {
+		stochastic_task *t;
+		t = t_sched->ts->task_list[j];
 
-	    if ((i % t->period) - t->activation_time == 0)
-	      {
-		go_ahead = 0;
-		current_task = j;
-		current_time = i;
-	      }
-	  }
+		if ((i % t->period) - t->activation_time == 0) {
+		    go_ahead = 0;
+		    current_task = j;
+		    current_time = i;
+		}
+	    }
 
-	task_copy = 0;
-      }
+	    task_copy = 0;
+	}
 
-    if (go_ahead)
-      {
-	t_sched->timeline.is_new = 1;
-	return 0;
-      }
-    else
-      {
-	t_sched->timeline.id = current_task;
-	t_sched->timeline.release_time = current_time;
-	return 1;
-      }
+	if (go_ahead) {
+	    t_sched->timeline.is_new = 1;
+	    return 0;
+	} else {
+	    t_sched->timeline.id = current_task;
+	    t_sched->timeline.release_time = current_time;
+	    return 1;
+	}
 
 
-  }
+    }
 
 }
 
 
 
-static double
-distr_mean (double *distribution, size_t distr_len)
+static double distr_mean(double *distribution, size_t distr_len)
 {
-  int i;
-  double mean = 0.0;
+    int i;
+    double mean = 0.0;
 
-  for (i = 0; i < distr_len; i++)
-    {
-      mean += i * distribution[i];
+    for (i = 0; i < distr_len; i++) {
+	mean += i * distribution[i];
     }
 
-  return mean;
+    return mean;
 
 }
 
 unsigned int
-get_max_idle_FP (tasks_schedule * t_sched, unsigned int kth_task)
+get_max_idle_FP(tasks_schedule * t_sched, unsigned int kth_task)
 {
-  unsigned int max_idle = 0;
-  task_timeline *tl;
+    unsigned int max_idle = 0;
+    task_timeline *tl;
 
-  tl = &t_sched->timeline;
-  max_idle += t_sched->hyperperiod;
+    tl = &t_sched->timeline;
+    max_idle += t_sched->hyperperiod;
 
-  if (kth_task >= t_sched->ts->tasks_num)
+
+    /*computing W_min */
     {
-      printf ("error! computing max idle of task %i but task len is %zu\n",
-	      kth_task, t_sched->ts->tasks_num);
+	unsigned int W_min = 0;
+	unsigned int prev_time, prev_duration;
+
+	do {
+	    next_task(t_sched);
+	}
+	while (has_priority_FP(t_sched->ts, kth_task, tl->id));
+
+	prev_time = t_sched->timeline.release_time;
+	prev_duration = min_index(&t_sched->ts->task_list[tl->id]->sd);
+	W_min = prev_duration;
+
+	while (next_task(t_sched)) {
+	    if (!has_priority_FP(t_sched->ts, kth_task, tl->id)) {
+		W_min = (W_min > tl->release_time - prev_time) ?
+		    W_min - (tl->release_time - prev_time) : 0;
+
+		prev_duration =
+		    min_index(&t_sched->ts->task_list[tl->id]->sd);
+
+		prev_time = t_sched->timeline.release_time;
+		W_min += prev_duration;
+	    }
+	}
+	W_min = (W_min > t_sched->hyperperiod - prev_time) ?
+	    W_min - (t_sched->hyperperiod - prev_time) : 0;
+
+	max_idle += W_min;
+    }
+    /*computing C_min */
+    {
+	unsigned int C_min = 0;
+	while (next_task(t_sched)) {
+	    if (!has_priority_FP(t_sched->ts, kth_task, tl->id)) {
+		C_min += min_index(&t_sched->ts->task_list[tl->id]->sd);
+	    }
+
+	}
+	max_idle -= C_min;
     }
 
-  /*computing W_min */
-  {
-    unsigned int W_min = 0;
-    unsigned int prev_time, prev_duration;
 
-    do
-      {
-	next_task (t_sched);
-      }
-    while (has_priority_FP (t_sched->ts, kth_task, tl->id));
-
-    prev_time = t_sched->timeline.release_time;
-    prev_duration =
-      min_index (t_sched->ts->task_list[tl->id]->sd.dist,
-		 t_sched->ts->task_list[tl->id]->sd.d_len);
-    W_min = prev_duration;
-
-    while (next_task (t_sched))
-      {
-	if (!has_priority_FP (t_sched->ts, kth_task, tl->id))
-	  {
-	    W_min = (W_min > tl->release_time - prev_time) ?
-	      W_min - (tl->release_time - prev_time) : 0;
-
-	    prev_duration =
-	      min_index (t_sched->ts->task_list[tl->id]->sd.dist,
-			 t_sched->ts->task_list[tl->id]->sd.d_len);
-
-	    prev_time = t_sched->timeline.release_time;
-	    W_min += prev_duration;
-	  }
-      }
-    W_min = (W_min > t_sched->hyperperiod - prev_time) ?
-      W_min - (t_sched->hyperperiod - prev_time) : 0;
-
-    max_idle += W_min;
-  }
-  /*computing C_min */
-  {
-    unsigned int C_min = 0;
-    while (next_task (t_sched))
-      {
-	if (!has_priority_FP (t_sched->ts, kth_task, tl->id))
-	  {
-	    C_min += min_index (t_sched->ts->task_list[tl->id]->sd.dist,
-				t_sched->ts->task_list[tl->id]->sd.d_len);
-	  }
-
-      }
-    max_idle -= C_min;
-  }
-
-
-  return max_idle;
+    return max_idle;
 }
 
 int
-has_priority_FP (stochastic_taskset * ts, unsigned int this,
-		 unsigned int over_this)
+has_priority_FP(stochastic_taskset * ts, unsigned int this,
+		unsigned int over_this)
 {
-  if (ts->task_list[this]->deadline < ts->task_list[over_this]->deadline)
-    return 1;
-  else
-    return 0;
-}
-
-
-
-static unsigned int
-min_index (double *distribution, size_t d_len)
-{
-  unsigned int i;
-
-  for (i = 0; i < d_len; i++)
-    {
-      if (distribution[i] > 0)
-	return i;
-    }
-
-  return 0;
+    if (ts->task_list[this]->deadline < ts->task_list[over_this]->deadline)
+	return 1;
+    else
+	return 0;
 }
--- src/schedulingutils.h
+++ src/schedulingutils.h
@@ -6,22 +6,20 @@
 #include <stdlib.h>
 
 /*struct that contains data to iterato over the hyperperiod on the taskset*/
-typedef struct
-{
-  int is_new;			/*is a new iteration */
-  unsigned int id;		/*id of the next task to activate */
-  unsigned int release_time;	/*releasing time WRT the hyperperiod */
+typedef struct {
+    int is_new;			/*is a new iteration */
+    unsigned int id;		/*id of the next task to activate */
+    int release_time;		/*releasing time WRT the hyperperiod */
 } task_timeline;
 
 /*
 struct representing a schedule of a taskset
 */
-typedef struct
-{
-  unsigned int hyperperiod;	/*hyperperiod */
-  unsigned int timeline_len;	/*number of tasks activated during a hyperperiod */
-  task_timeline timeline;
-  stochastic_taskset *ts;
+typedef struct {
+    unsigned int hyperperiod;	/*hyperperiod */
+    unsigned int timeline_len;	/*number of tasks activated during a hyperperiod */
+    task_timeline timeline;
+    stochastic_taskset *ts;
 } tasks_schedule;
 
 
@@ -30,37 +28,41 @@ typedef struct
 /*
 returns a task schedule object for the stochastic taskset "ts"
 */
-tasks_schedule *new_task_schedule (stochastic_taskset * ts);
+tasks_schedule *new_task_schedule(stochastic_taskset * ts);
 
 /*
 frees the memory of the task schedule passed AND THE ASSOCIATED TASKSET
 */
-void free_task_schedule (tasks_schedule * t_sched);
+void free_task_schedule(tasks_schedule * t_sched);
 
 /*
 Check if the system is stable (if the average system utilization is < 1)
 */
-int is_stable (tasks_schedule * t_sched);
+int is_stable(tasks_schedule * t_sched);
+/*
+return average utilization for the given task set
+*/
+double get_average_utilization(stochastic_taskset * ts);
 
 /*iterates over the tasks of a task_schedule over the hyperperiod,
 setting the timeline struct inside t_sched to the id of the
 next job that has to start and the relative (respect to the
 hyperperiod) starting time*/
-int next_task (tasks_schedule * t_sched);
+int next_task(tasks_schedule * t_sched);
 
 /*
 check in a taskset with fixed priorities
 task "this" has priority respect to the task "over_this".
 */
-int has_priority_FP (stochastic_taskset * ts, unsigned int this,
-		     unsigned int over_this);
+int has_priority_FP(stochastic_taskset * ts, unsigned int this,
+		    unsigned int over_this);
 /*
 gets the maximum idle time << called "r" inside the paper >>
 of a taskset (inside a task schedule)
 with fixed priorities, respect to the kth task of the taskset.
 */
-unsigned int get_max_idle_FP (tasks_schedule * t_sched,
-			      unsigned int kth_task);
+unsigned int get_max_idle_FP(tasks_schedule * t_sched,
+			     unsigned int kth_task);
 
 
 
